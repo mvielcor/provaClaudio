d725bc3bc93ce46d02e97812171414baa4506d61
Manel <pmdm2dam@gmail.com>
2018-09-25 18:35:16 +0200

    Initial commit

---
+++ app/src/main/java/pmdm/manel/prova/ClaudioActivity.java
@@ -0,0 +1,13 @@
+package pmdm.manel.prova;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ClaudioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_claudio);
+    }
+}
